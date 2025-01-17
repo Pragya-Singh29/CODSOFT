80e515618e3105f9fdafdb23ecfda575e4d42729
Pragya-Singh29 <pragyaasingh1618@gmail.com>
2025-01-17 14:02:45 +0530

    Added new files

--- Task2.java
+++ Task2.java
@@ -1,3 +1,5 @@
 public class Task2 {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
 }
---
+++ Task3.java
@@ -0,0 +1,6 @@
+public class Task3 {
+    public static void main(String[] args) {
+        System.out.println("helloooooooooooooooooooooooooooooooooooooooooooooooooo");
+        System.out.println("helloooooooooooooooooooooooooooooooooooooooooooooooooo");
+    }
+}
